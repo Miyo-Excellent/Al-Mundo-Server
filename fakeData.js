12617f509bf7cc99ebeb4360ef01bda3585b5075
Michell Excellent Marin <theofficesmichell@gmail.com>
2018-11-19 11:21:33 -0500

    V0.0.1

---
+++ fakeData.js
@@ -0,0 +1,76 @@
+export default {
+  hotels: [
+    {
+      name: 'Hotel Emperador',
+      stars: 0,
+      image: 'http://static.asiawebdirect.com/m/kl/portals/malacca-ws/homepage/accommodation/top10-luxury-hotels/pagePropertiesImage/malacca-top10-luxury.jpg',
+      price: 1596,
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    },
+    {
+      name: 'Hotel Emperador 2',
+      stars: 1,
+      image: 'https://www.croatiaweek.com/wp-content/uploads/2017/01/Royal-Blue-Hotel.jpg?x46277',
+      price: 2485,
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    },
+    {
+      name: 'Hotel Emperador 3',
+      stars: 2,
+      image: 'https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2014/09/21/1512/CHEJU-P306-Outdoor-Swimming-Pool.adapt.16x9.1920.1080.jpg',
+      price: 3374,
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    },
+    {
+      name: 'Hotel Emperador 4',
+      stars: 3,
+      image: 'https://www.weinstrassenhotels.com/typo3temp/pics/weinegg-II-WEB-66_60e2a519ff.jpg',
+      price: 4263,
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    },
+    {
+      name: 'Hotel Emperador 5',
+      stars: 4,
+      image: 'http://www.jebelsifah.com/wp-content/uploads/2016/10/hotels.jpg',
+      price: 5152,
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    },
+    {
+      name: 'Hotel Emperador 6',
+      stars: 5,
+      image: 'https://cdn.asiatatler.com/asiatatler/my/i/2018/03/story/40851/07165903-new-world-hotel-2-c07165952-1584x779.jpg',
+      price: 6942,
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    }
+  ]
+};
